9ea293edb4dce50639276684eea35ce29e16f99f
Victor Shyba <victor.shyba@gmail.com>
2016-04-20 19:45:04 -0300

    Adds endpoints for connecting both agents

--- routes/conference.rb
+++ routes/conference.rb
@@ -1,12 +1,15 @@
 module Routes
   module Conference
+
+    AGENT_WAIT_URL = 'http://twimlets.com/holdmusic?Bucket=com.twilio.music.classical'
+
     def self.registered(app)
       app.post '/conference/connect/client' do
         agent_id = 'agent1'
         conference_id = params[:CallSid]
         Caller.call_agent(
           agent_id,
-          ''
+          "/conference/#{conference_id}/connect/agent1/"
         )
 
         twiml = TwimlGenerator.generate_connect_conference(
@@ -22,6 +25,37 @@ module Routes
         content_type 'text/xml'
         twiml
       end
+
+      app.post '/conference/:conference_id/connect/agent1/' do
+        content_type 'text/xml'
+        TwimlGenerator.generate_connect_conference(
+          params[:conference_id],
+          AGENT_WAIT_URL,
+          true,
+          false
+        )
+      end
+
+      app.post '/conference/:conference_id/connect/agent2/' do
+        content_type 'text/xml'
+        TwimlGenerator.generate_connect_conference(
+          params[:conference_id],
+          AGENT_WAIT_URL,
+          true,
+          true
+        )
+      end
+
+      app.post '/conference/:agent_id/call' do
+        agent_id = params[:agent_id]
+        conference_id = ActiveCall.first(agent_id: agent_id).conference_id
+
+        Caller.call_agent(
+          'agent2',
+          "/conference/#{conference_id}/connect/agent2/"
+        )
+      end
+
       app.post '/conference/wait' do
         content_type 'text/xml'
         TwimlGenerator.generate_wait
--- spec/requests/conference_spec.rb
+++ spec/requests/conference_spec.rb
@@ -1,28 +1,79 @@
 require_relative '../spec_helper'
 
-describe 'POST /conference/connect/client' do
-  it 'is successful' do
-    call_sid = '1234'
-    expect(TwimlGenerator).to receive(:generate_connect_conference)
-      .with(call_sid, '/conference/wait', false, true)
-      .once
-      .and_return('<Response></Response>')
+describe 'Conference routes'do
 
-    expect(Caller).to receive(:call_agent)
-      .with("agent1", '')
-      .once
-    post 'conference/connect/client', CallSid: call_sid
-    expect(last_response).to be_ok
+  agent_wait_url = 'http://twimlets.com/holdmusic?Bucket=com.twilio.music.classical'
+  conference_id = 'conference1234'
+
+  before(:each) do
+    ActiveCall.all.destroy
   end
-end
-describe 'POST /conference/wait' do
-  it 'returns Dial Conference TwiML' do
-    expect(TwimlGenerator).to receive(:generate_wait)
-      .once
-      .and_return('<Response></Response>')
 
-    post '/conference/wait'
+  describe 'POST /conference/connect/client' do
+    it 'returns Dial Conference TwiML' do
+      call_sid = '1234'
+      expect(TwimlGenerator).to receive(:generate_connect_conference)
+        .with(call_sid, '/conference/wait', false, true)
+        .once
+        .and_return('<Response></Response>')
+  
+      expect(Caller).to receive(:call_agent)
+        .with("agent1", "/conference/#{call_sid}/connect/agent1/")
+        .once
+      post 'conference/connect/client', CallSid: call_sid
+      expect(last_response).to be_ok
+    end
+  end
 
-    expect(last_response).to be_ok
+  describe 'POST /conference/:conference_id/connect/agent1/' do
+    it 'returns Dial Conference TwiML' do
+      expect(TwimlGenerator).to receive(:generate_connect_conference)
+        .with(conference_id, agent_wait_url, true, false)
+        .once
+        .and_return('<Response></Response>')
+  
+      post "/conference/#{conference_id}/connect/agent1/"
+  
+      expect(last_response).to be_ok
+    end
+  end
+
+  describe 'POST /conference/:conference_id/connect/agent2/' do
+    it 'returns Dial Conference TwiML' do
+  
+      expect(TwimlGenerator).to receive(:generate_connect_conference)
+        .with(conference_id, agent_wait_url, true, true)
+        .once
+        .and_return('<Response></Response>')
+  
+      post "/conference/#{conference_id}/connect/agent2/"
+  
+      expect(last_response).to be_ok
+    end
+  end
+
+  describe 'POST /conference/:agent_id/call' do
+    it 'creates a call to agent 2' do
+      ActiveCall.create(agent_id: 'agent1', conference_id: conference_id)
+      expect(Caller).to receive(:call_agent)
+        .with('agent2', "/conference/#{conference_id}/connect/agent2/")
+        .once
+
+      post "conference/agent1/call"
+  
+      expect(last_response).to be_ok
+    end
+  end
+
+  describe 'POST /conference/wait' do
+    it 'returns Dial Conference TwiML' do
+      expect(TwimlGenerator).to receive(:generate_wait)
+        .once
+        .and_return('<Response></Response>')
+  
+      post '/conference/wait'
+
+      expect(last_response).to be_ok
+    end
   end
 end
