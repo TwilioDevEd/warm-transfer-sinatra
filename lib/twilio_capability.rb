80f117f0b596ea5ce27a76d68c0a68bd7f77562a
mosampaio <mosampaio@gmail.com>
2016-10-13 18:42:35 -0300

    Bump Twilio Library to Version 5.x

--- lib/twilio_capability.rb
+++ lib/twilio_capability.rb
@@ -2,7 +2,7 @@ module TwilioCapability
   def self.generate(agent_id)
     account_sid = ENV['TWILIO_ACCOUNT_SID']
     auth_token = ENV['TWILIO_AUTH_TOKEN']
-    capability = Twilio::Util::Capability.new(account_sid, auth_token)
+    capability = Twilio::JWT::Capability.new(account_sid, auth_token)
 
     capability.allow_client_incoming(agent_id)
     capability.generate
